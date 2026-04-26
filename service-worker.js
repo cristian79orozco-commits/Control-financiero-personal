589dc3a32c5ea99ec3895fe61c1be4b92bc4cac7
cristian orozco <cristian79orozco@gmail.com>
2026-04-26 16:07:56 -0500

    habilitacion y modificacion de codigos para crear APK

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,37 @@
-const CACHE_NAME = 'finanzas-v1';
-const ARCHIVOS = ['/', '/index.html'];
+const CACHE_NAME = 'finanzas-v3';
+const BASE = '/Control-financiero-personal/';
 
+const ARCHIVOS = [
+    BASE,
+    BASE + 'index.html',
+    BASE + 'manifest.json',
+    BASE + 'icon-192.png',
+    BASE + 'icon-512.png'
+];
+
+// INSTALAR
 self.addEventListener('install', e => {
     e.waitUntil(
         caches.open(CACHE_NAME).then(cache => cache.addAll(ARCHIVOS))
     );
 });
 
+// ACTIVAR
+self.addEventListener('activate', e => {
+    e.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+            )
+        )
+    );
+});
+
+// FETCH
 self.addEventListener('fetch', e => {
     e.respondWith(
-        caches.match(e.request).then(respuesta => respuesta || fetch(e.request))
+        caches.match(e.request).then(res => {
+            return res || fetch(e.request).catch(() => caches.match(BASE + 'index.html'));
+        })
     );
 });
